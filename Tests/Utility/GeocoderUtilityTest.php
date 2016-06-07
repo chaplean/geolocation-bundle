52a7cecd268018340b7f24ae74c8e62b2161bc89
Valentin Bontemps <valentin@chaplean.com>
2016-06-07 08:06:18 +0200

    #14994 optimize search lat/lng by Address

--- Tests/Utility/GeocoderUtilityTest.php
+++ Tests/Utility/GeocoderUtilityTest.php
@@ -70,11 +70,93 @@ class GeoLocationUtilityTest extends LogicalTest
                 ),
             ), 'OK'));
 
-        /** @var GeocoderResponse $response */
         $result = $this->geocoder->getLongitudeLatitudeByAddress('9 rue de condé, 33000, Bordeaux');
 
-        $this->assertEquals(44.8435849, round($result['latitude'], 7));
-        $this->assertEquals(-0.5733138, round($result['longitude'], 7));
+        static::assertEquals(44.8435849, round($result['latitude'], 7));
+        static::assertEquals(-0.5733138, round($result['longitude'], 7));
+    }
+
+    /**
+     * @return void
+     * @throws \Exception
+     */
+    public function testfindLongitudeLatitudeByAddressWithGoodAddress()
+    {
+        $this->ivoryGeocoderMock->shouldReceive('geocode')
+            ->andReturn(new GeocoderResponse(array(
+                new GeocoderResult(
+                    array(),
+                    '9 Rue de Condé, 33000 Bordeaux, France',
+                    new GeocoderGeometry(
+                        new Coordinate(44.8435849, -0.5733138, true),
+                        'ROOFTOP',
+                        new Bound(
+                            new Coordinate(44.8435849, -0.5733138, true),
+                            new Coordinate(44.8435849, -0.5733138, true),
+                            array()
+                        ),
+                        null
+                    ),
+                    array(
+                        'street_address'
+                    ),
+                    null
+                ),
+            ), 'OK'));
+
+        $address = new Address();
+        $address->setBlock1('9 rue de condé');
+        $address->setBlock2('');
+        $address->setCity('Bordeaux');
+        $address->setZipcode('33000');
+
+        $result = $this->geocoder->findLongitudeLatitudeByAddress($address);
+
+        static::assertEquals(44.8435849, round($result['latitude'], 7));
+        static::assertEquals(-0.5733138, round($result['longitude'], 7));
+    }
+
+
+    /**
+     * @return void
+     * @throws \Exception
+     */
+    public function testfindLongitudeLatitudeByAddressWithSubAddressNotFound()
+    {
+        $this->ivoryGeocoderMock->shouldReceive('geocode')
+            ->andReturn(new GeocoderResponse(array(), 'ZERO_RESULTS'))->once();
+        $this->ivoryGeocoderMock->shouldReceive('geocode')
+            ->andReturn(new GeocoderResponse(array(
+                new GeocoderResult(
+                    array(),
+                    '1 Place de la Pyramide, 92800 Puteaux, France',
+                    new GeocoderGeometry(
+                        new Coordinate(48.8895717, 2.2420858, true),
+                        'ROOFTOP',
+                        new Bound(
+                            new Coordinate(48.888222669708, 2.2407368197085, true),
+                            new Coordinate(48.890920630291, 2.2434347802915, true),
+                            array()
+                        ),
+                        null
+                    ),
+                    array(
+                        'street_address'
+                    ),
+                    null
+                ),
+            ), 'OK'));
+
+        $address = new Address();
+        $address->setBlock1('Tour Atlantique');
+        $address->setBlock2('1, place de la pyramide ');
+        $address->setCity('Paris La Défense');
+        $address->setZipcode('92911');
+
+        $result = $this->geocoder->findLongitudeLatitudeByAddress($address);
+
+        static::assertEquals(48.8895717, round($result['latitude'], 7));
+        static::assertEquals(2.2420858, round($result['longitude'], 7));
     }
 
     /**
@@ -90,6 +172,23 @@ class GeoLocationUtilityTest extends LogicalTest
 
         /** @var GeocoderResponse $response */
         $this->geocoder->getLongitudeLatitudeByAddress(', , ');
+    }
+
+    /**
+     * @return void
+     *
+     * @expectedException \Exception
+     * @expectedExceptionMessage ZERO_RESULTS
+     */
+    public function testGetLongitudeLatitudeByAddressWithNotFoundAddress()
+    {
+        $this->ivoryGeocoderMock->shouldReceive('geocode')
+            ->andReturn(new GeocoderResponse(array(), 'ZERO_RESULTS'));
+
+        $address = new Address();
+
+        /** @var GeocoderResponse $response */
+        $this->geocoder->findLongitudeLatitudeByAddress($address);
     }
 
     /**
@@ -129,9 +228,27 @@ class GeoLocationUtilityTest extends LogicalTest
 
         $address = $this->geocoder->getAddress('9 rue de condé, 33000, Bordeaux');
 
-        $this->assertInstanceOf(Address::class, $address);
-        $this->assertEquals('9 Rue de Condé', $address->getBlock1());
-        $this->assertEquals('33000', $address->getZipcode());
-        $this->assertEquals('Bordeaux', $address->getCity());
+        static::assertInstanceOf(Address::class, $address);
+        static::assertEquals('9 Rue de Condé', $address->getBlock1());
+        static::assertEquals('33000', $address->getZipcode());
+        static::assertEquals('Bordeaux', $address->getCity());
     }
+//    public function testFindLongitudeLatitude()
+//    {
+////        $address = new Address();
+////        $address->setBlock1('Tour Atlantique');
+////        $address->setBlock2('1, place de la pyramide ');
+////        $address->setCity('Paris La Défense');
+////        $address->setZipcode('92911');
+////
+////        $result = $this->geocoder->findLongitudeLatitudeByAddress($address);
+////
+////        $address = new Address();
+////        $address->setBlock1('17 rue du Bois Briand');
+////        $address->setBlock2('CS 83589');
+////        $address->setCity('NANTES cedex 3');
+////        $address->setZipcode('44335');
+////
+////        $result = $this->geocoder->findLongitudeLatitudeByAddress($address);
+//    }
 }

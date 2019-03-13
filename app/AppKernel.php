6caa18a8099ced7fbcfe483aba4884728a280216
Matthias Devlamynck <matthias@chaplean.coop>
2019-03-13 12:04:38 +0100

    #32568, Update dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,7 +27,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Chaplean\Bundle\UnitBundle\ChapleanUnitBundle(),
             new Liip\FunctionalTestBundle\LiipFunctionalTestBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Cravler\MaxMindGeoIpBundle\CravlerMaxMindGeoIpBundle(),
             new Chaplean\Bundle\GeolocationBundle\ChapleanGeolocationBundle(),
             new FOS\RestBundle\FOSRestBundle(),
